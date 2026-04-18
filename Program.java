1f817e88a770b62385a756b9db1ab1c14e40ba66
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-04-18 17:12:13 +0800

    list: established a list array

--- Program.java
+++ Program.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import models.DogSex;
 
 public class Program {
 
@@ -9,37 +12,44 @@ public class Program {
      public static class Animal extends Organism {
         protected String breed = "Labrador";
     }
+
+    public static ArrayList<Dog> animals = new ArrayList<Dog>();
     
     public static class Dog extends Animal {
         private String breed;
         private String name;
+        private DogSex sex;
 
-
-        public Dog(String name) {
-            this("Shitzu", name);
+        public Dog(String name, DogSex sex) {
+            this("Shitzu", name, sex);
         }
 
-        public Dog(String breed, String name) {
-             
+        public Dog(String breed, String name, DogSex sex) {
+            this.breed = breed;
             this.name = name;
-            this.breed = super.breed;
-           
+            this.sex = sex;
         }
-        
 
          public void greet() {
             System.out.println("Hi I am " + name + " and I'm a " + breed);
         }
+
+      /*   public void sayGender() {
+            System.out.println("My gender is " + sex);
+        } */
+
        
     }
 
     public static void main(String[] args) {
         
-        Scanner scan = new Scanner(System.in);
-        
-        Dog dog = new Dog("Tonio");
-     
-        dog.greet();
+       Dog dog1 = new Dog("Labrador", "BaoBao", DogSex.MALE);
+       Dog dog2 = new Dog("Luna", DogSex.FEMALE);
+       animals.add(dog1);
+       animals.add(dog2);
+       Dog dog3 = new Dog("Labrador", "BaoBao", DogSex.MALE);
+       System.out.println(animals.contains(dog3));
+
 
     }
 
