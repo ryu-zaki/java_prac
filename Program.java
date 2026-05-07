2e83b51d2a14b862eeb33b7cad779f2e2e83fa12
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-05-07 23:59:59 +0800

    composite: get and set

--- Program.java
+++ Program.java
@@ -8,8 +8,9 @@ public class Program {
     public static void main(String[] args) throws InterruptedException {
         // RandomNumber.setIsTest();
       //  DatabaseConnection.runThreads();
-        FileSystemElement = new FileSystemElement();
-        
+        FileSystemElement file = new FileSystemElement("");
+        file.getName();
+                file.setName();
         Randomizer.runThreads();
     }
 
