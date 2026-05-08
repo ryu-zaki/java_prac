5ddd34f05d88bf06c5e297d252478200a515b38e
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-05-08 22:24:29 +0800

    composite: established files for composite entities

--- menu/MenuCategory.java
+++ menu/MenuCategory.java
@@ -1,4 +1,39 @@
 package menu;
 
-public class MenuCategory {
+import java.util.ArrayList;
+import java.util.List;
+
+public class  MenuCategory extends  MenuComponent {
+
+    private List<MenuComponent> items;
+
+    public MenuCategory(String name) {
+        super(name);
+        this.items = new ArrayList<>();
+    }
+
+    @Override
+    public void display(String prepend) {
+
+        System.out.println(prepend + "*-- " + getName());
+
+        for (MenuComponent item: items) {
+            item.display("  " + prepend);
+        }
+
+    }
+
+    @Override
+    public void add(MenuComponent newItem) {
+        items.add(newItem);
+    }
+
+    @Override
+    public void remove(String name) {
+        this.items =
+        items.stream()
+                .filter(item -> !item.getName().equals(name))
+                .toList();
+    }
+
 }
